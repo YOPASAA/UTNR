193b49e13be05a603ba3c27b9d81edf2b415621f
YOPASAA <ivanyopasa@gmail.com>
2025-03-28 14:15:21 -0500

    ultra.py

--- ultra.py
+++ ultra.py
@@ -16,26 +16,29 @@ st.set_page_config(
         # "About": "Aplicación de análisis de ventas creada con Streamlit 🚀"
     }
 )
-
 st.title("Bienvenido al Validador 🗂️")
 st.sidebar.header("Menú de Navegación")
 
 db_filename = "Base_Pacientes_NAL_BOT.bd"
 file_id = "1o8CFlVb0HERuErdoNz6iXl9MMgzzQuvX"
 url = f"https://drive.google.com/uc?id={file_id}"
-
 gdown.download(url, db_filename, quiet=False)
 
 conn = sqlite3.connect(db_filename)
-
 consulta = st.text_input("Ingrese el número de identificación")
 
 if consulta:
     if consulta.isdigit():  # Asegura que el ID es un número
-        query = "SELECT PERIODO, VOLANTE, TRASLADOS_AUTORIZADOS, DISPONIBLES, COD_AXSEG, MIPRES, COORDINACIÓN FROM mi_tabla WHERE ID = ?"
-        df = pd.read_sql_query(query, conn, params=(consulta,))
+
         query_2 = "SELECT NOMBRE, TIPO_ID, PROGRAMA, CIUDAD FROM mi_tabla WHERE ID = ?"
         df_2 = pd.read_sql_query(query_2, conn, params=(consulta,))
+        
+        query = "SELECT PERIODO, VOLANTE, TRASLADOS_AUTORIZADOS, DISPONIBLES, COD_AXSEG, MIPRES, COORDINACIÓN FROM mi_tabla WHERE ID = ?"
+        df = pd.read_sql_query(query, conn, params=(consulta,))
+
+        query_3 = "SELECT COORDINACIÓN FROM mi_tabla JOIN coordinacion ON COORDINACIÓN = COORDINACIÓN ?"
+        df_3 = pd.read_sql_query(query, conn, params=(consulta))
+        
         if df.empty:
             st.warning("No se encontraron resultados.")
         else:
@@ -44,6 +47,7 @@ if consulta:
             tipo = df_2["TIPO_ID"].values[0]
             st.subheader(f"👤{tipo}-{consulta} | {nombre} | {ciudad}")
             st.dataframe(df, use_container_width=True)
+            st.dataframe(df_3, use_container_width=True)
     else:
         st.error("Por favor, ingrese un ID numérico válido.")
 conn.close()
