a705d75d117061218348285444a55fe6e468a540
YOPASAA <ivanyopasa@gmail.com>
2025-03-28 15:31:47 -0500

    ultra.py

--- ultra.py
+++ ultra.py
@@ -1,260 +1,69 @@
-from google.oauth2 import service_account
-from googleapiclient.discovery import build
+import streamlit as st
 import pandas as pd
-import pyfiglet
-from datetime import datetime
-from googleapiclient.errors import HttpError
-import time
-import os
+import gdown
 import sqlite3
+import os
+import time
 
-# Función para manejar la consulta con reintentos
-def get_sheet_data(service, spreadsheet_id, range_name, retries=3):
-    print('Iniciando consulta...')
-    start_time_total = time.time()  # Inicia el cronómetro total
-    for attempt in range(retries):
-        attempt_start_time = time.time()  # Inicia cronómetro del intento
-        try:
-            print(f'🔄 Intento {attempt + 1} de {retries}')            
-            result = service.spreadsheets().values().get(
-                spreadsheetId=spreadsheet_id, range=range_name
-            ).execute()            
-            duration = time.time() - attempt_start_time  # Tiempo del intento
-            total_duration = time.time() - start_time_total  # Tiempo total            
-            print(f'✅ Datos obtenidos en intento {attempt + 1}. '
-                  f'Tiempo: {duration:.2f} seg | Total: {total_duration:.2f} seg')
-            return result.get('values', [])        
-        except HttpError as e:
-            duration = time.time() - attempt_start_time  # Tiempo del intento
-            if e.resp.status == 503:
-                if attempt < retries - 1:
-                    wait_time = 5 * (2 ** attempt)
-                    print(f'⚠️ Error 503 en intento {attempt + 1} ({duration:.2f} seg). '
-                          f'Reintentando en {wait_time} segundos')
-                    time.sleep(wait_time)  # Espera exponencial
-                else:
-                    total_duration = time.time() - start_time_total
-                    print(f'❌ Error 503 después de {retries} intentos ({total_duration:.2f} seg) en {spreadsheet_id}: {e}')
-                    raise e
-            else:
-                print(f'❌ Error inesperado en {spreadsheet_id}: {e}')
-                raise e
-        except Exception as e:
-            print(f'❌ Error inesperado en {spreadsheet_id}: {e}')
-            raise e
-    total_duration = time.time() - start_time_total
-    print(f'❌ No se pudo obtener datos de {spreadsheet_id} después de {retries} intentos. Tiempo total: {total_duration:.2f} seg')
-    return []
+st.set_page_config(
+    page_title="Asistente UTNR",
+    page_icon="🤖",
+    layout="wide",
+    initial_sidebar_state="collapsed",
+    menu_items={
+        "Get Help": "https://miweb.com/ayuda"
+        # "Report a bug": "https://miweb.com/soporte",
+        # "About": "Aplicación de análisis de ventas creada con Streamlit 🚀"
+    }
+)
+st.title("Bienvenido al Validador 🗂️")
+st.sidebar.header("Menú de Navegación")
 
-# Ruta al archivo JSON de credenciales descargado
-credentials_path = 'C:/IY/TNR/Factura/facturacion1-406819-1fb1d31473b2.json'
-credentials = service_account.Credentials.from_service_account_file(credentials_path, scopes=['https://www.googleapis.com/auth/spreadsheets'])
-service = build('sheets', 'v4', credentials=credentials)
+db_filename = "Base_Pacientes_NAL_BOT.bd"
+file_id = "1o8CFlVb0HERuErdoNz6iXl9MMgzzQuvX"
+url = f"https://drive.google.com/uc?id={file_id}"
+gdown.download(url, db_filename, quiet=False)
 
-#Obtener datos de la hoja de Google de BOGOTA
-id_bogota = '1qW7n1-MCRtzY4KHWry32qXrKGPHe2f3Wb7jyIzFWz8o'
-try:
-    print('\n---BOGOTA')
-    valuesBTA = get_sheet_data(service, id_bogota, 'BASE PACIENTES!A:Q')
-    BTA = pd.DataFrame(valuesBTA[1:], columns=valuesBTA[0])
-    BTA.loc[:, 'REGIONAL'] = 'BOGOTA'
-    DP_BTA = BTA[['REGIONAL',
-                  'PERIODO',
-                  'PERIODO',
-                  'Tipo ID',
-                  'ID',
-                  'Apellidos y Nombres del Usuario',
-                  'MIPRES/SADOR',
-                  'VOLANTE', 
-                  'REGIMEN',
-                  'PROGRAMA',   
-                  'GESTIÓN DE EXCEDIDOS',
-                  'CIUDAD DEL AFILIADO',
-                  'Cantidad de traslados VALIDADOR',
-                  'Código Aseguradora VALIDADOR'
-                  ]]
-except Exception as e:
-    print(f'(X) BTA {e}')
-    
-#Obtener datos de la hoja de Google de MEDELLIN
-id_medellin ='1nFaComfyKXjZp_QVDESqbbdspmfZ8Hqb6RoKZtS4ZaA'
-try:
-    print('\n---MEDELLIN')
-    valuesMDL = get_sheet_data(service, id_medellin, 'BASE PACIENTES!A:AC')
-    MDL = pd.DataFrame(valuesMDL[1:], columns=valuesMDL[0])
-    MDL.loc[:, 'REGIONAL'] = 'MEDELLIN'
-    DP_MDL = MDL[['REGIONAL',
-                  'PERIODO',
-                  'PERIODO',
-                  'Tipo Identificación',
-                  'ID',
-                  'NOMBRE DEL PACIENTE',
-                  '# PRESCRIPCION',
-                  'NUMERO DE VOLANTE',
-                  'REGIMEN',
-                  'PROGRAMA',
-                  'DISPONIBLES',
-                  'CIUDAD DEL AFILIADO',
-                  'Traslados Autorizados',
-                  'Código Aseguradora Original Validador',
-                  ]]
-except Exception as e:
-    print(f'(X) MDL {e}')
+conn = sqlite3.connect(db_filename)
+consulta = st.text_input("Ingrese el número de identificación")
 
-#Obtener datos de la hoja de Google de COSTA
-id_costa ='1i7yLN0-Qkp2EMm2y_LArUXIvqgOXpyRuhMPavdqWM08'
-try:
-    print('\n---COSTA')
-    valuesCST = get_sheet_data(service, id_costa, 'BASE PACIENTES!A:AD')
-    CST = pd.DataFrame(valuesCST[1:], columns=valuesCST[0])
-    CST.loc[:, 'REGIONAL'] = 'COSTA'
-    DP_CST = CST[['REGIONAL',
-                  'PERIODO',
-                  'PERIODO',
-                  'Tipo Identificación',
-                  'ID',
-                  'Apellidos y Nombres del Usuario',
-                  '# PRESCRIPCION',
-                  'NUMERO DE VOLANTE',
-                  'REGIMEN',
-                  'TIPO',
-                  'GESTIÓN DE EXCEDIDOS',
-                  'CIUDAD DEL AFILIADO',
-                  'Cantidad de traslados solicitados Mes',
-                  'Código Aseguradora',
-                  ]]
-except Exception as e:
-    print(f'(X) CST {e}')
+if consulta:
+    if consulta.isdigit():  # Asegura que el ID es un número
+        query = "SELECT PERIODO, VOLANTE, TRASLADOS_AUTORIZADOS, DISPONIBLES, COD_AXSEG, MIPRES, COORDINACIÓN FROM mi_tabla WHERE ID = ?"
+        df = pd.read_sql_query(query, conn, params=(consulta,))
 
-#Obtener datos de la hoja de Google de OCCIDENTE
-id_occidente ='1gFIlk55p6TSBeJOsb4gO-zyIce6NGDy0FYjyPhV7XMo'
-try:
-    print('\n---OCCIDENTE')
-    valuesOCC = get_sheet_data(service, id_occidente, 'BASE PACIENTES!A:AH')
-    OCC = pd.DataFrame(valuesOCC[1:], columns=valuesOCC[0])
-    OCC.loc[:, 'REGIONAL'] = 'OCCIDENTE'
-    DP_OCC = OCC[['REGIONAL',
-                  'PERIODO',
-                  'PERIODO',
-                  'Tipo ID',
-                  'ID',
-                  'Apellidos y Nombres del Usuario',
-                  'PRESCRIPCION',
-                  'VOLANTE',
-                  'REGIMEN',
-                  'PROGRAMA',
-                  'CONTACTOS',
-                  'CIUDAD DEL AFILIADO',
-                  'Cantidad de traslados VALIDADOR',
-                  'Código Aseguradora VALIDADOR',
-                  ]]
-except Exception as e:
-    print(f'(X) OCC {e}')
-    
-#Obtener datos de la hoja de Google de CUCUTA
-id_cucuta ='133kjROBgPX3GxTqbK1Tv_TV9LQ8xwKKfqZxPV2CejXs'
-try:
-    print('\n---CUCUTA')
-    valuesCTA = get_sheet_data(service, id_cucuta, 'BASE PACIENTES!A:AM')
-    CTA = pd.DataFrame(valuesCTA[1:], columns=valuesCTA[0])
-    CTA.loc[:, 'REGIONAL'] = 'CUCUTA'
-    DP_CTA = CTA[['REGIONAL',
-                  'PERIODO',
-                  'PERIODO',
-                  'Tipo ID',
-                  'ID',
-                  'Apellidos y Nombres del Usuario',
-                  'PRESCRIPCION',
-                  'VOLANTE',
-                  'REGIMEN',
-                  'PROGRAMA',
-                  'GESTIÓN DE EXCEDIDOS',
-                  'CIUDAD DEL AFILIADO',
-                  'Cantidad de traslados VALIDADOR',
-                  'Código Aseguradora VALIDADOR',
-                  ]]
-except Exception as e:
-    print(f'(X) CTA {e}')
-    
-#Obtener datos de la hoja de Google de BUCARAMANGA
-id_bucaramanga = '1_DW63p0VCivyIDRrbL7rh8JyfYbng1kJNGZ-M4wbgn4'
-try:
-    print('\n---BUCARAMANGA')
-    valuesBCA = get_sheet_data(service, id_bucaramanga, 'BASE PACIENTES!A:Q')
-    BCA = pd.DataFrame(valuesBCA[1:], columns=valuesBCA[0])
-    BCA.loc[:, 'REGIONAL'] = 'BUCARAMANGA/ALDÑ'
-    DP_BCA = BCA[['REGIONAL',
-                  'PERIODO',
-                  'PERIODO',
-                  'Tipo ID',
-                  'ID',
-                  'Apellidos y Nombres del Usuario',
-                  'PRESCRIPCION',
-                  'VOLANTE',
-                  'REGIMEN',
-                  'PROGRAMA',
-                  'GESTIÓN DE EXCEDIDOS',
-                  'CIUDAD DEL AFILIADO',
-                  'Cantidad de traslados VALIDADOR',
-                  'Código Aseguradora VALIDADOR',
-                  ]]
-except Exception as e:
-    print(f'(X) BCA {e}')
-    
-new_names = ['COORDINACIÓN', 
-             'PERIODO',
-             'AÑO',
-             'TIPO_ID',
-             'ID',
-             'NOMBRE',
-             'MIPRES',
-             'VOLANTE',
-             'REGIMEN',
-             'PROGRAMA',
-             'DISPONIBLES',
-             'CIUDAD',
-             'TRASLADOS_AUTORIZADOS',
-             'COD_AXSEG'
-             ]
+        query_2 = """
+        SELECT
+            mi_tabla.NOMBRE,
+            mi_tabla.TIPO_ID,
+            mi_tabla.PROGRAMA,
+            mi_tabla.CIUDAD,
+            coordinacion.Link
+        FROM mi_tabla
+        LEFT JOIN coordinacion
+        ON mi_tabla.COORDINACIÓN = coordinacion.COORDINACIÓN
+        WHERE mi_tabla.ID = ?
+        """
+        df_2 = pd.read_sql_query(query_2, conn, params=(consulta,))
 
-DP_BCA.columns = new_names
-DP_MDL.columns = new_names
-DP_CST.columns = new_names
-DP_BTA.columns = new_names
-DP_OCC.columns = new_names
-DP_CTA.columns = new_names
-
-base_pacientes = pd.concat([DP_BTA, DP_BCA, DP_MDL, DP_CST, DP_OCC, DP_CTA], ignore_index=True)
-base_pacientes = base_pacientes.dropna(subset=['ID'])
-base_pacientes = base_pacientes.query("ID != ''")
-base_pacientes['ID'] = pd.to_numeric(base_pacientes['ID'], errors='coerce').astype('Int64')
-fecha_actual = datetime.now().strftime('%Y-%m-%d')
-base_pacientes.to_excel(r'G:\Mi unidad\UNIDAD_YVAN\Base_Pacientes_NAL\Base_Pacientes_NAL_' + fecha_actual + '.xlsx', index=False)
-
-#GENERAR BASE DEL BOT
-periodo = fecha_actual.split('-')
-mes_actual = int(periodo[1])
-periodo = periodo[0]+periodo[1].zfill(2)
-
-if mes_actual == 12:
-    periodo_final = int(periodo) + 89  # Para diciembre
-else:
-    periodo_final = int(periodo) + 1   # Para demas meses
-
-base_bot = base_pacientes[
-    (base_pacientes['PERIODO'] == str(periodo)) | 
-    (base_pacientes['PERIODO'] == str(periodo_final))]
-
-# Eliminar el archivo si ya existe 
-if os.path.exists(r'G:\Mi unidad\UNIDAD_YVAN\Base_Pacientes_NAL\Base_Pacientes_NAl_BOT.xlsx'): 
-    os.remove(r'G:\Mi unidad\UNIDAD_YVAN\Base_Pacientes_NAL\Base_Pacientes_NAL_BOT.xlsx')
-
-base_bot.to_excel(r'G:\Mi unidad\UNIDAD_YVAN\Base_Pacientes_NAL\Base_Pacientes_NAL_BOT.xlsx', index=False)
-text = "!OK Base Pacientes y Base BOT!"
-ascii_art = pyfiglet.figlet_format(text)
-
-conn = sqlite3.connect('Base_Pacientes_NAL_BOT.bd')
-base_bot.to_sql('mi_tabla', conn, if_exists="replace", index=False)
+        if df.empty:
+            st.warning("No se encontraron resultados.")
+        else:
+            nombre = df_2["NOMBRE"].values[0]
+            ciudad = df_2["CIUDAD"].values[0]
+            tipo = df_2["TIPO_ID"].values[0]
+            coord = df["COORDINACIÓN"].values[0]
+            link = df_2["Link"].values[0]
+            st.subheader(f"👤{tipo}-{consulta} | {nombre} | {ciudad}")
+            st.dataframe(df, use_container_width=True) 
+            st.markdown(f"📌 **Puedes programar tus servicios en el siguiente link:** [Ir al enlace]({link})")
+    else:
+        st.error("Por favor, ingrese un ID numérico válido.")
 conn.close()
 
-print(ascii_art)
+
+            # col1, col2, col3 = st.columns([1, 1, 1])
+            # col1.markdown(f"👤 Nombre: {nombre}")  # Izquierda
+            # col2.markdown(f"📍 Ciudad: {ciudad}")  # Centro
+            # col3.markdown(f"🎓 Programa: {programa}")  # Derecha
+            
