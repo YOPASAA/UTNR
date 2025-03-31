bae3a11bc7b74887a5545729523c00340f735e77
YOPASAA <ivanyopasa@gmail.com>
2025-03-31 18:35:03 -0500

    horarios.py

--- pages/horarios.py
+++ pages/horarios.py
@@ -1,25 +1,4 @@
 import streamlit as st
-import pandas as pd
-import gdown
-import sqlite3
-import urllib.parse
-import os
-import time
-
-st.set_page_config(
-    page_title="Asistente UTNR",
-    page_icon="🤖",
-    layout="wide",
-    initial_sidebar_state="collapsed",
-    menu_items={
-        "Get Help": "https://miweb.com/ayuda"
-        # "Report a bug": "https://miweb.com/soporte",
-        # "About": "Aplicación de análisis de ventas creada con Streamlit 🚀"
-    }
-)
-
-st.sidebar.header("Menú de Navegación")
-
 
 col1,col2 = st.columns([1,8])
 with col1:
