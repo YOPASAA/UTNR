781a2ff5fadbc7d7adc979b82671f0b987e81ba5
YOPASAA <ivanyopasa@gmail.com>
2025-03-31 21:36:48 -0500

    horarios.py

--- pages/horarios.py
+++ pages/horarios.py
@@ -48,4 +48,4 @@ col1, col2 = st.columns([1,1])
 with col1:
   st.link_button("Visitar: https://nuevorenetur.com.co/","https://nuevorenetur.com.co", use_container_width=True)
 with col2:
-  sr.write("")
+  st.write("")
