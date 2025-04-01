a705853341113703b48364fbf52e0d8d1f93f824
YOPASAA <ivanyopasa@gmail.com>
2025-03-31 19:34:44 -0500

    horarios.py

--- pages/horarios.py
+++ pages/horarios.py
@@ -1,1 +1,11 @@
+import streamlit as st
+
+st.sidebar.title("Menú de Navegación")
+
+col1,col2 = st.columns([1,8])
+with col1:
+    st.image("Imagen1.png", width=150)  
+with col2:
+    st.write("")  # Espacio en blanco
+    st.title("Pagina 2 perra")
 
